fee81432260cc7186cd560b404f50fc35f061ced
azax25547 <momentumangular@gmail.com>
2020-04-26 00:51:09 +0530

    setting ipaddress for every server

--- fcc-2.js
+++ fcc-2.js
@@ -1,14 +1,23 @@
 const express = require("express");
+const os = require("os");
 const app = express();
 
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
+const nwInterface = os.networkInterfaces();
+let ip = "";
 
+if (nwInterface.hasOwnProperty("eth0")) {
+  ip = nwInterface.eth0[0].address;
+} else if (nwInterface.hasOwnProperty("Loopback Pseudo-Interface 1")) {
+  ip = nwInterface["Loopback Pseudo-Interface 1"][1].address;
+}
+app.set("trust proxy", true);
 app.get("/api/whoami", (req, res) => {
-  //   console.log(req.headers);
+  //   console.log(nwInterface);
   res.json({
-    ipaddress: req.ip,
+    ipaddress: ip,
     language: req.headers["accept-language"],
     software: req.headers["user-agent"],
   });
